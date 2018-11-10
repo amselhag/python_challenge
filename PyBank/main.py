9f29aab783875b04a88c0004861a4fff8c81abc4
amselhag <amselhag@gmail.com>
2018-11-10 13:50:55 -0600

    added comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,17 @@
 import os
 import csv
+
+#finding the path to the file
 csvpath=os.path.join('budget_data.csv')
+
+#opening the CSV file
 
 with open(csvpath, newline='') as csvfile:
 
     reader=csv.reader(csvfile, delimiter=',')
     csv_header = next(reader)
    
-    
+   #creating two lists months and profit_losses to store each column
     months= []
     profit_losses=[]
     
@@ -15,14 +19,16 @@ with open(csvpath, newline='') as csvfile:
         months.append(rows[0])
         profit_losses.append(int(rows[1]))
         
+#calculated the number of rows by taking the len of the months list
 
 Total_months= len(months)
 Total= sum(profit_losses)
 
 
-
+#finding the maximum from the porfit losses column
 maximum = max((profit_losses))
-#print (maximum)
+
+#created a new list called monthly_change
 
 monthly_change=[]
 
@@ -33,13 +39,10 @@ for i in range (1, Total_months):
     
 
 average_change= sum(monthly_change)/len(monthly_change)
-#print(len(monthly_change))
-#print (average_change)
+
 greatest_increase= max(monthly_change)
 greatest_decrease=min(monthly_change)
 
-#print(greatest_increase)
-#print (greatest_decrease)
 
 index_max = monthly_change.index(greatest_increase)
 index_min= monthly_change.index(greatest_decrease)
@@ -48,10 +51,7 @@ index_min= monthly_change.index(greatest_decrease)
 greatest_month=months[index_max+1]
 lowest_month= months[index_min+1]
 
-#print(greatest_month)
-#print (lowest_month)
-
-
+#printing the results to the terminal
 print("   finacial analysis    ")
 print("---------------------------")
 print(f'Total Months: {Total_months}')
@@ -61,7 +61,7 @@ print(f'Greatest Increase in Profits: {greatest_month[3:]} (${greatest_increase}
 print(f'Greatest decrease in Profits: {lowest_month [3:]} (${greatest_decrease})')
 
 
-
+#creating a text file and saving the results
 with open('out.txt', 'w') as output:
     print("   finacial analysis    ", file=output)
     print("---------------------------", file=output)
