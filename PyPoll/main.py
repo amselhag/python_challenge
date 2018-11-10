113a1d45c59987081b527f9f0458429ec6724bda
amselhag <amselhag@gmail.com>
2018-11-10 13:31:38 -0600

    added comments to the file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,16 @@
 import os
 import csv
 
+#selectin the path 
 election_data_csvpath= os.path.join('election_data.csv')
 
+#opening the CSV file
 with open(election_data_csvpath, newline='') as csvfile:
 
     election= csv.reader(csvfile, delimiter= ',')
     election_header= next(election)
    
+ #convert each column to a list
     
     voter_ID=[]
     county=[]
@@ -18,13 +21,12 @@ with open(election_data_csvpath, newline='') as csvfile:
         county.append(rows[1])
         candidate.append(rows[2])
         
-#print(voter_ID)
-#print(county)
-#print(candidate)
+
+#calculate the total votes and change it to integer
 
 total_votes=int(len(voter_ID))
 
-
+#creating a new list called candidates that take unique values of the candidates 
 candidates=[]
 
 for items in candidate:
@@ -32,19 +34,20 @@ for items in candidate:
         candidates.append(items)
         
 
-        
+#creating 2 lists one with voting results and the percentages by using the count function
 voting_results=[]
 voting_results_percent = []
+
 
 for i in range (0, len(candidates)):
     
     voting_results.append(candidate.count(candidates[i]))
     voting_results_percent.append(candidate.count(candidates[i])/total_votes*100)
 
-
-rounded_percentage=[]
-
+#finding the index value of the max to obtain the winner
 winner_index = voting_results.index(max(voting_results))
+
+#print the results
 
 print('Election Results')
 print("-----------------------")
@@ -56,6 +59,7 @@ print("-----------------------")
 print (f'winner: {candidates[winner_index]}')
 print("-----------------------")
 
+#open a text file and prin the results to that file
 with open('out.txt', 'w') as output:
     print('Election Results', file=output)
     print("-----------------------", file=output)
